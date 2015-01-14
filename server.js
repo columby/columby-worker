1d5c37a9c4a30da7f8e2c6609fefe75ebdbec5e4
A. van der Pluijm <info@urbanlink.nl>
2015-01-14 13:24:05 +0100

    basic auth
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- server.js
+++ server.js
@@ -3,26 +3,55 @@
 
 // modules
 var express = require('express'),
+    auth = require('basic-auth'),
     kue = require('kue'),
 	  request = require('request'),
     cors = require('cors');
 
+var config = require('./config/settings');
+
 var arcgisProcessor = require('./processors/arcgisProcessor');
 var fortesProcessor = require('./processors/fortesProcessor');
 
+var app = express();
+
+
+/**
+ * Basic authentication for UI
+ */
+app.use(function(req, res, next) {
+  var user = auth(req);
+
+  if (user === undefined || user['name'] !== config.auth.username || user['pass'] !== config.auth.password) {
+    res.statusCode = 401;
+    res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+    res.end('Unauthorized');
+  } else {
+    next();
+  }
+});
+
+
+/**
+ * Middleware
+ */
+app.use(cors());
+
+
+/**
+ * Initiate Kue
+ */
 var jobs = kue.createQueue();
 
 jobs.process('arcgis', function(job,done){ arcgisProcessor(job,done); });
 jobs.process('fortes', function(job,done){ fortesProcessor(job,done); });
 
-
-var app = express();
-
-app.use(cors());
 app.use(kue.app);
 app.set('title', 'Columby Worker // kue');
 
+
+// Start the server 
 var server = require('http').createServer(app);
-server.listen(process.env.COLUMBY_WORKER_PORT || 7000, function(){
-  console.log('Kue started.');
+server.listen(config.port, function(){
+  console.log('Kue started on port ' + config.port);
 });
